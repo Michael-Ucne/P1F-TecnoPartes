e78d46c943d7b3a53ebcdc3de7baf6d4c8f5b24d
Michael <michael_jose@ucne.edu.do>
2025-07-28 18:44:01 -0400

    Correcion de Program.cs

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAntiforgery();
-app.MapStaticAssets();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
